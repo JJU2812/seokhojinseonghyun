63cae750cf90a553c9bff0b471751f7a892650f4
junu <jeongjunu208@gmail.com>
2025-10-24 19:59:59 +0900

    simsim

--- day1.py
+++ day1.py
@@ -1,1 +1,11 @@
-print("민규")
+import random
+
+dice = random.randint(1,6)
+answer1 = int(input("민규주사위\n"))
+answer2 = int(input("준우주사위\n"))
+if dice == answer1:
+    print("민규정답")
+elif dice == answer2:
+    print("준우정답")
+else:
+    print("오답")
